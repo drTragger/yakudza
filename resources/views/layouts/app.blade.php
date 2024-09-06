0b0a8bb02c678ec72100012679f15eda42ec9c21
Mykhailo Ponomarenko <mishaponomarenko11082001@gmail.com>
2024-09-06 14:22:22 +0300

    Added meta pixel code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,6 +43,8 @@
             'https://connect.facebook.net/en_US/fbevents.js');
         fbq('init', '1241125230249235');
         fbq('track', 'PageView');
+        fbq('track', 'CompleteRegistration');
+        fbq('track', 'Lead');
     </script>
     <noscript><img height="1" width="1" style="display:none"
                    src="https://www.facebook.com/tr?id=1241125230249235&ev=PageView&noscript=1"
